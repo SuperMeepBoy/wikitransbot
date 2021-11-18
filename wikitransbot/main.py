98197a152e6ec1c79ad3538c011f181d8fa48e28
Jef Roelandt <roelandt.jef@protonmail.com>
2021-11-18 10:41:41 +0100

    Move code in a separate folder

--- wikitransbot/main.py
+++ wikitransbot/main.py
@@ -42,7 +42,7 @@ def check_mentions(api, keywords, since_id, config):
 
 
 def main():
-    config = json.load(open('config.json', 'r', encoding='utf-8'))
+    config = json.load(open('../config.json', 'r', encoding='utf-8'))
     twitter_config = config['twitter']
     last_id_file = config['last_id_file']
 
