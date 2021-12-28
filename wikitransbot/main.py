36f913c3541e8c7b5dad381d5b85859cee9811ca
Jef Roelandt <roelandt.jef@protonmail.com>
2021-12-28 13:36:54 +0100

    Use Twitter API V2

--- wikitransbot/main.py
+++ wikitransbot/main.py
@@ -3,7 +3,6 @@ import requests
 import time
 
 import tweepy
-import tweepy.error
 
 
 def get_since_id(file_path):
@@ -14,32 +13,35 @@ def get_since_id(file_path):
         return 1
 
 
-def check_mentions(api, keywords, since_id, config):
+def check_mentions(client, keywords, wikitransbot_id, since_id, config):
     new_since_id = since_id
-    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
-        new_since_id = max(tweet.id, new_since_id)
-        tweet_text = tweet.text.split(' ')
+    tweets = client.get_users_mentions(wikitransbot_id, since_id=since_id)
+    if tweets.data:
+        for tweet in tweets.data:
+            new_since_id = max(tweet.id, new_since_id)
+            tweet_text = tweet.text.split(' ')
 
-        if tweet_text[1] != config['trigger_keyword']:
-            continue
+            if tweet_text[1] != config['trigger_keyword']:
+                continue
 
-        url = "https://wikitrans.co/wp-admin/admin-ajax.php?action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D="
-        request_url = f"{url}{' '.join(tweet_text[2:])}"
+            base_url = "https://wikitrans.co/wp-admin/admin-ajax.php"
+            parameters = "?action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D="
+            url = base_url + parameters
+            request_url = f"{url}{' '.join(tweet_text[2:])}"
 
-        response = requests.get(request_url)
-        if response.status_code == 200 and not tweet.favorited:
-            data = response.json()['data']
-            if data['post_count'] == 0:
-                api.update_status(
-                    config['no_answer_template'] % (tweet.author.screen_name),
-                    tweet.id
-                )
-            else:
-                api.update_status(
-                    config['answer_template'] % (tweet.author.screen_name, data['posts'][0]['link']),
-                    tweet.id,
-                )
-            tweet.favorite()
+            response = requests.get(request_url)
+            if response.status_code == 200:
+                data = response.json()['data']
+                if data['post_count'] == 0:
+                    client.create_tweet(
+                        text=config['no_answer_template'],
+                        in_reply_to_tweet_id=tweet.id,
+                    )
+                else:
+                    client.create_tweet(
+                        text=config['answer_template'] % (data['posts'][0]['link']),
+                        in_reply_to_tweet_id=tweet.id,
+                    )
     return new_since_id
 
 
@@ -48,15 +50,19 @@ def main():
     twitter_config = config['twitter']
     last_id_file = config['last_id_file']
 
-    # Twitter things
-    auth = tweepy.OAuthHandler(twitter_config['twitter_api_key'], twitter_config['twitter_api_key_secret'])
-    auth.set_access_token(twitter_config['twitter_access_token'], twitter_config['twitter_access_token_secret'])
-    api = tweepy.API(auth)
+    client = tweepy.Client(
+        consumer_key=twitter_config['twitter_api_key'],
+        consumer_secret=twitter_config['twitter_api_key_secret'],
+        access_token=twitter_config['twitter_access_token'],
+        access_token_secret=twitter_config['twitter_access_token_secret'],
+        bearer_token=twitter_config['twitter_bearer_token'],
+    )
 
+    wikitransbot_id = 1457666990554894337
     since_id = get_since_id(last_id_file)
 
     while True:
-        since_id = check_mentions(api, ["article"], since_id, config)
+        since_id = check_mentions(client, ["article"], wikitransbot_id, since_id, config)
         with open(last_id_file, 'w') as f:
             f.write(str(since_id))  # So if the bot crashes we know where to start
         print("Sleeping")
