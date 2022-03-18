b112e38bb50996eab0cd7d719eb1cde92dc25c88
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-18 11:35:15 +0100

    Fix: get mentions in reply to other tweets or replies
    Changed the library used to access Twitter API as tweepy did not return mentions in reply to other tweets or replies.

--- wikitransbot/main.py
+++ wikitransbot/main.py
@@ -3,7 +3,7 @@ import logging
 import requests
 import time
 
-import tweepy
+from pytwitter import Api
 
 
 def get_since_id(file_path):
@@ -14,36 +14,38 @@ def get_since_id(file_path):
         return 1
 
 
-def check_mentions(client, keywords, wikitransbot_id, since_id, config):
+def check_mentions(api, keywords, wikitransbot_id, since_id, config):
     try:
         new_since_id = since_id
-        tweets = client.get_users_mentions(wikitransbot_id, since_id=since_id)
-        if tweets.data:
-            for tweet in tweets.data:
-                new_since_id = max(tweet.id, new_since_id)
-                tweet_text = tweet.text.split(' ')
+        tweets = api.get_mentions(user_id=wikitransbot_id, since_id=since_id).data
+        for tweet in tweets:
+            new_since_id = max(tweet.id, new_since_id)
+            splitted_tweet = tweet.text.split(config['trigger_keyword'] + ' ')
 
-                if tweet_text[1] != config['trigger_keyword']:
-                    continue
+            # Trigger word not found
+            if len(splitted_tweet) == 1:
+                continue
 
-                base_url = "https://wikitrans.co/wp-admin/admin-ajax.php"
-                parameters = "?action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D="
-                url = base_url + parameters
-                request_url = f"{url}{' '.join(tweet_text[2:])}"
+            base_url = "https://wikitrans.co/wp-admin/admin-ajax.php"
+            parameters = "?action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D="
+            url = base_url + parameters
+            request_url = f"{url}{' '.join(splitted_tweet[1])}"
 
-                response = requests.get(request_url)
-                if response.status_code == 200:
-                    data = response.json()['data']
-                    if data['post_count'] == 0:
-                        client.create_tweet(
-                            text=config['no_answer_template'],
-                            in_reply_to_tweet_id=tweet.id,
-                        )
-                    else:
-                        client.create_tweet(
-                            text=config['answer_template'] % (data['posts'][0]['link']),
-                            in_reply_to_tweet_id=tweet.id,
-                        )
+            response = requests.get(request_url)
+            if response.status_code == 200:
+                data = response.json()['data']
+                if data['post_count'] == 0:
+                    api.create_tweet(
+                        text=config['no_answer_template'],
+                        reply_in_reply_to_tweet_id=tweet.id,
+                        reply_exclude_reply_user_ids=[],
+                    )
+                else:
+                    api.create_tweet(
+                        text=config['answer_template'] % (data['posts'][0]['link']),
+                        reply_in_reply_to_tweet_id=tweet.id,
+                        reply_exclude_reply_user_ids=[],
+                    )
         return new_since_id
 
     except Exception as e:
@@ -56,19 +58,18 @@ def main():
     twitter_config = config['twitter']
     last_id_file = config['last_id_file']
 
-    client = tweepy.Client(
+    api = Api(
         consumer_key=twitter_config['twitter_api_key'],
         consumer_secret=twitter_config['twitter_api_key_secret'],
         access_token=twitter_config['twitter_access_token'],
         access_token_secret=twitter_config['twitter_access_token_secret'],
-        bearer_token=twitter_config['twitter_bearer_token'],
     )
 
     wikitransbot_id = 1457666990554894337
     since_id = get_since_id(last_id_file)
 
     while True:
-        since_id = check_mentions(client, ["article"], wikitransbot_id, since_id, config)
+        since_id = check_mentions(api, ["article"], wikitransbot_id, since_id, config)
         with open(last_id_file, 'w') as f:
             f.write(str(since_id))  # So if the bot crashes we know where to start
         print("Sleeping")
