002d7c681b7deab741bd8035d16185e59cdba90d
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-18 14:01:01 +0100

    Refacto main function of the bot

--- tests/wikitransbot/test_main.py
+++ tests/wikitransbot/test_main.py
@@ -1,5 +1,7 @@
 import tempfile
 
+import pytest
+
 from wikitransbot import main as wt_bot
 
 
@@ -8,3 +10,31 @@ def test_get_since_id():
         tmp_file.write(b'1000')
         tmp_file.flush()
         assert(wt_bot.get_since_id(tmp_file.name) == 1000)
+
+
+@pytest.mark.parametrize("tweet_text, keyword, expected_result", [
+    (
+        "@wikitransbot article féminiser sa voix",
+        "article",
+        (
+            "https://wikitrans.co/wp-admin/admin-ajax.php?"
+            "action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D=féminiser sa voix"
+        ),
+    ),
+    (
+        "@ally @wikitransbot article psy transfriendly",
+        "article",
+        (
+            "https://wikitrans.co/wp-admin/admin-ajax.php?"
+            "action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D=psy transfriendly"
+        ),
+    ),
+    (
+        "@wikitransbot tu as perdu",
+        "article",
+        "",
+    ),
+])
+def test_build_search_article_url(tweet_text, keyword, expected_result):
+    result = wt_bot.build_search_article_url(tweet_text=tweet_text, keyword=keyword)
+    assert(result == expected_result)
