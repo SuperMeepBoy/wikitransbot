63009f61c25264d2f8ee34bf43fe76ac87bff1ed
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-19 09:29:04 +0100

    Technical: switch bot to Object Oriented Programming

--- tests/wikitransbot/test_main.py
+++ tests/wikitransbot/test_main.py
@@ -1,40 +1,48 @@
+from unittest.mock import patch
 import tempfile
 
 import pytest
 
-from wikitransbot import main as wt_bot
+from wikitransbot.main import Bot
 
 
-def test_get_since_id():
-    with tempfile.NamedTemporaryFile() as tmp_file:
-        tmp_file.write(b'1000')
-        tmp_file.flush()
-        assert(wt_bot.get_since_id(tmp_file.name) == 1000)
+class TestBot:
 
+    def test_get_since_id(self):
+        with patch.object(Bot, '__init__', lambda x: None):
+            with tempfile.NamedTemporaryFile() as tmp_file:
+                bot = Bot()
+                bot.since_id_file_path = tmp_file.name
+                tmp_file.write(b'1000')
+                tmp_file.flush()
+                assert(bot.get_since_id() == 1000)
 
-@pytest.mark.parametrize("tweet_text, keyword, expected_result", [
-    (
-        "@wikitransbot article féminiser sa voix",
-        "article",
+    @pytest.mark.parametrize("tweet_text, keyword, expected_result", [
         (
-            "https://wikitrans.co/wp-admin/admin-ajax.php?"
-            "action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D=féminiser sa voix"
+            "@wikitransbot article féminiser sa voix",
+            "article",
+            (
+                "https://wikitrans.co/wp-admin/admin-ajax.php?"
+                "action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D=féminiser sa voix"
+            ),
         ),
-    ),
-    (
-        "@ally @wikitransbot article psy transfriendly",
-        "article",
         (
-            "https://wikitrans.co/wp-admin/admin-ajax.php?"
-            "action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D=psy transfriendly"
+            "@ally @wikitransbot article psy transfriendly",
+            "article",
+            (
+                "https://wikitrans.co/wp-admin/admin-ajax.php?"
+                "action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D=psy transfriendly"
+            ),
         ),
-    ),
-    (
-        "@wikitransbot tu as perdu",
-        "article",
-        "",
-    ),
-])
-def test_build_search_article_url(tweet_text, keyword, expected_result):
-    result = wt_bot.build_search_article_url(tweet_text=tweet_text, keyword=keyword)
-    assert(result == expected_result)
+        (
+            "@wikitransbot tu as perdu",
+            "article",
+            "",
+        ),
+    ])
+    def test_build_search_article_url(self, tweet_text, keyword, expected_result):
+        with patch.object(Bot, '__init__', lambda x: None):
+            bot = Bot()
+            bot.keyword = keyword
+            result = bot.build_search_article_url(tweet_text=tweet_text)
+            assert(result == expected_result)
--- wikitransbot/main.py
+++ wikitransbot/main.py
@@ -6,82 +6,86 @@ import time
 from pytwitter import Api
 
 
-def get_since_id(file_path):
-    try:
-        with open(file_path, 'r') as f:
-            return int(f.read())
-    except FileNotFoundError:  # 1st time bot in ran
-        return 1
+class Bot:
 
+    def __init__(self):
+        self.config = json.load(open('/etc/wikitransbot/config.json', 'r', encoding='utf-8'))
 
-def build_search_article_url(*, tweet_text, keyword):
-    splitted_tweet = tweet_text.split(keyword + ' ')
+        # TODO: move  in conf ?
+        self.wikitransbot_id = 1457666990554894337
+        self.api = self.get_twitter_api()
+        self.since_id_file_path = self.config['last_id_file']
+        self.since_id = self.get_since_id()
+        self.keyword = self.config['trigger_keyword']
+        self.sleep_time = self.config['sleep_time']
 
-    # Trigger word not found
-    if len(splitted_tweet) == 1:
-        return ""
+    def get_twitter_api(self):
+        twitter_config = self.config['twitter']
+        return Api(
+            consumer_key=twitter_config['twitter_api_key'],
+            consumer_secret=twitter_config['twitter_api_key_secret'],
+            access_token=twitter_config['twitter_access_token'],
+            access_secret=twitter_config['twitter_access_token_secret'],
+        )
 
-    base_url = "https://wikitrans.co/wp-admin/admin-ajax.php"
-    parameters = "?action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D="
-    url = base_url + parameters
-    return f"{url}{splitted_tweet[1]}"
+    def get_since_id(self):
+        try:
+            with open(self.since_id_file_path, 'r') as f:
+                return int(f.read())
+        except FileNotFoundError:  # 1st time bot in ran
+            return 1
 
+    def build_search_article_url(self, *, tweet_text):
+        splitted_tweet = tweet_text.split(self.keyword + ' ')
 
-def run(api, keyword, wikitransbot_id, since_id, config):
-    try:
-        new_since_id = since_id
-        tweets = api.get_mentions(user_id=wikitransbot_id, since_id=since_id).data
-        for tweet in tweets:
-            new_since_id = max(int(tweet.id), new_since_id)
+        # Trigger word not found
+        if len(splitted_tweet) == 1:
+            return ""
 
-            request_url = build_search_article_url(tweet_text=tweet.text, keyword=keyword)
-            if not request_url:
-                continue
+        base_url = "https://wikitrans.co/wp-admin/admin-ajax.php"
+        parameters = "?action=jet_ajax_search&search_taxonomy%5D=&data%5Bvalue%5D="
+        url = base_url + parameters
+        return f"{url}{splitted_tweet[1]}"
 
-            response = requests.get(request_url)
-            if response.status_code == 200:
-                data = response.json()['data']
-                if data['post_count'] == 0:
-                    api.create_tweet(
-                        text=config['no_answer_template'],
-                        reply_in_reply_to_tweet_id=tweet.id,
-                        reply_exclude_reply_user_ids=[],
-                    )
-                else:
-                    api.create_tweet(
-                        text=config['answer_template'] % (data['posts'][0]['link']),
-                        reply_in_reply_to_tweet_id=tweet.id,
-                        reply_exclude_reply_user_ids=[],
-                    )
-        return new_since_id
+    def tweet(self, *, text, to):
+        self.api.create_tweet(
+            text=text,
+            reply_in_reply_to_tweet_id=to,
+            reply_exclude_reply_user_ids=[],
+        )
 
-    except Exception as e:
-        logging.info(str(e))
-        return since_id
-
-
-def main():
-    config = json.load(open('/etc/wikitransbot/config.json', 'r', encoding='utf-8'))
-    twitter_config = config['twitter']
-    last_id_file = config['last_id_file']
-
-    api = Api(
-        consumer_key=twitter_config['twitter_api_key'],
-        consumer_secret=twitter_config['twitter_api_key_secret'],
-        access_token=twitter_config['twitter_access_token'],
-        access_secret=twitter_config['twitter_access_token_secret'],
-    )
-
-    wikitransbot_id = 1457666990554894337
-    since_id = get_since_id(last_id_file)
-
-    while True:
-        since_id = run(api, config['trigger_keyword'], wikitransbot_id, since_id, config)
-        with open(last_id_file, 'w') as f:
-            f.write(str(since_id))  # So if the bot crashes we know where to start
+    def sleep(self):
+        time.sleep(self.sleep_time)
         print("Sleeping")
-        time.sleep(config['sleep_time'])
+
+    def run(self):
+        while True:
+            try:
+                tweets = self.api.get_mentions(user_id=self.wikitransbot_id, since_id=self.since_id).data
+                for tweet in tweets:
+                    new_since_id = max(int(tweet.id), self.since_id)
+
+                    request_url = self.build_search_article_url(tweet_text=tweet.text)
+                    if not request_url:
+                        continue
+
+                    response = requests.get(request_url)
+                    if response.status_code == 200:
+                        data = response.json()['data']
+                        if data['post_count'] == 0:
+                            self.tweet(text=self.config['no_answer_template'], to=tweet.id)
+                        else:
+                            self.tweet(text=self.config['answer_template'] % (data['posts'][0]['link']), to=tweet.id)
+                self.since_id = new_since_id
+                with open(self.since_id_file_path, 'w') as f:
+                    f.write(str(new_since_id))  # So if the bot crashes we know where to start
+
+            except Exception as e:
+                logging.info(str(e))
+
+            self.sleep()
 
 
 if __name__ == "__main__":
-    main()
+    bot = Bot()
+    bot.run()
